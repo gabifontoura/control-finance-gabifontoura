71082a0a3299ca6fe3e4b2bf93d31d4f7e5772ac
Gabriela <gabrielagarciafontoura@gmail.com>
2022-09-30 15:08:06 -0300

    first-commit

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,48 @@
-/* Desenvolva sua lógica aqui */
+// Preciso:
+//1 listar liMoneyMovement com o array de objetos insertedValues - OK
+//2 criar liMoneyMovement quando clicar em btn inserir (do modal) com info do input(value) e do valueType - ok
+//3 fazer condiçao no valueType para selecionar apenas entrada ou apenas Saída, deixando o btn com estilo active - ok
+//4 filtar entradas, saídas e todos -ok
+//5 remover liMoneyMovement pelo iconTrash - ok
+//6 remover liMoneyMovement do array insertedValues -ok
+//7 somar valores movimentados na conta
+
+
+
+
+
+function totalSum (array) {
+
+
+    // let total = insertedValues.reduce((acumulador,valorAtual) => {
+    //     return acumulador + valorAtual.value
+    // },0)
+
+    const entradas = insertedValues.filter((element) => element.categoryID == 1 )
+
+    let totalEntradas = entradas.reduce((acumulador,valorAtual) => {
+        return acumulador + valorAtual.value
+    },0)
+
+    const saidas = insertedValues.filter((element) => element.categoryID == 2)
+
+    let totalSaidas = saidas.reduce((acumulador,valorAtual) => {
+        return acumulador + valorAtual.value
+    },0)
+
+    
+    let sum = document.getElementById("sum-movements")
+
+    sum.innerText = ""
+    sum.innerText = `R$${(totalEntradas - totalSaidas).toFixed(2)}`
+
+
+  
+}
+
+totalSum(insertedValues)
+
+
+
+
+
